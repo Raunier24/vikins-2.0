8b14ff91b773af3f0a871b31c5260101e70bbb54
Raunier24 <peplisoft@gmail.com>
2024-08-14 20:46:00 +0200

    Class Saxon done

--- src/main/java/org/example/saxon/Saxon.java
+++ src/main/java/org/example/saxon/Saxon.java
@@ -1,12 +1,34 @@
 package org.example.saxon;
 
-public class Saxon {
+import org.example.soldier.Soldier;
 
-    int health = 60;
-    int strength = 25;
-    Saxon saxon = new Saxon(health, strength);
+public class Saxon extends Soldier {
+    private String msg;
 
-    assertNotNull(saxon 
+    // Constructor que recibe health y strength
+    public Saxon(int health, int strength) {
+        super(health, strength); // Llama al constructor de Soldier
+    }
 
-instanceof Soldier);
+    // Método attack que devuelve la propiedad strength
+    public int attack() {
+        return this.strength; // Retorna la fuerza del Saxon
+    }
+
+    // Método receiveDamage que resta el daño recibido de la salud
+    public void receiveDamage(int damage) {
+        super.receiveDamage(damage); // Llama a receiveDamage de Soldier
+
+        // Verifica si la salud es mayor a 0 para determinar el mensaje
+        if (this.health > 0) {
+            this.msg = "A Saxon has received " + damage + " points of damage";
+        } else {
+            this.msg = "A Saxon has died in combat";
+        }
+    }
+
+    // Método que retorna el mensaje guardado
+    public String getMsg() {
+        return this.msg;
+    }
 }
